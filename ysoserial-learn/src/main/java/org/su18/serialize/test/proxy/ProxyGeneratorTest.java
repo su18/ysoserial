1d771bc55b06dfdb154b48dedac49700f8964b5e
phoebe <phoebebuffayfan@foxmail.com>
2021-07-29 09:52:59 +0800

    更新目录结构，更新 Spring 反序列化测试代码

--- ysoserial-learn/src/main/java/org/su18/serialize/test/proxy/ProxyGeneratorTest.java
+++ ysoserial-learn/src/main/java/org/su18/serialize/test/proxy/ProxyGeneratorTest.java
@@ -5,7 +5,6 @@ import sun.misc.ProxyGenerator;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 /**
  * 生成代理类的几种写法
