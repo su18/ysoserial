cc05ca719d3ea89e9fe2c072b223daba675f8d89
phoebe <phoebebuffayfan@foxmail.com>
2021-08-31 15:37:50 +0800

    注释 GetterTest 代码

--- ysoserial-learn/src/main/java/org/su18/serialize/test/GetterTest.java
+++ ysoserial-learn/src/main/java/org/su18/serialize/test/GetterTest.java
@@ -1,7 +1,7 @@
 package org.su18.serialize.test;
 
 import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
-import org.su18.serialize.utils.SerializeUtil;
+//import org.su18.serialize.utils.SerializeUtil;
 // For hibernate 4
 // import org.hibernate.property.BasicPropertyAccessor;
 // import org.hibernate.property.Getter;
@@ -16,7 +16,7 @@ public class GetterTest {
 	public static void main(String[] args) throws Exception {
 
 		// 生成包含恶意类字节码的 TemplatesImpl 类
-		TemplatesImpl tmpl = SerializeUtil.generateTemplatesImpl();
+//		TemplatesImpl tmpl = SerializeUtil.generateTemplatesImpl();
 
 		// For hibernate 4
 //		BasicPropertyAccessor bpa    = new BasicPropertyAccessor();
